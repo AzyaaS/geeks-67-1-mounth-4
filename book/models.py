0b28261c8cfd657bb6c865a70989a435a1dc830a
Azyaa <saaz.sr.rs@gmail.com>
2026-08-18 02:34:21 +0600

    дз 2

--- book/models.py
+++ book/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)      
+    image = models.ImageField(upload_to='book/' )
+    year = models.IntegerField()  # год создания      
+    pages = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    age = models.IntegerField() # возрастное ограничение 
+    url_blog = models.URLField(blank=True) # ссылка на литрес
+    created_at = models.DateField(auto_now_add=True)  # Дата добавления
+    
+
+    def __str__(self):
+        return self.title
--- book/views.py
+++ book/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-# Create your views here.
+from .models import Book
+
+def book_list_view(request):
+    if request.method == 'GET':
+        books_lst = Book.objects.all().order_by('-id')
+    return render(request, 'book_lst.html', {'books_lst': books_lst})
+
+def book_detail_view(request, id):
+    if request.method == 'GET':
+        book_id = get_object_or_404(Book, id=id)
+    return render(request, 'book_detail.html', {'book_id': book_id})
+
+
 
 def my_favourite_book(request):
     if request.method == 'GET':

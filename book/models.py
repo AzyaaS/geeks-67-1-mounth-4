daac383fe964f545e1a954f6dddd7b5ea8680ed4
Azyaa <saaz.sr.rs@gmail.com>
2026-08-18 06:50:47 +0600

    дз 6

--- book/models.py
+++ book/models.py
@@ -4,14 +4,14 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)      
-    image = models.ImageField(upload_to='book/' )
-    year = models.IntegerField()  # год создания      
+    image = models.ImageField(upload_to='book/')
+    year = models.IntegerField()
     pages = models.IntegerField()
     genre = models.CharField(max_length=50)
-    age = models.IntegerField() # возрастное ограничение 
-    url_blog = models.URLField(blank=True) # ссылка на литрес
-    created_at = models.DateField(auto_now_add=True)  # Дата добавления
-    
+    age = models.IntegerField()
+    url_blog = models.URLField(blank=True)
+    created_at = models.DateField(auto_now_add=True)
+    views = models.PositiveIntegerField(default=0, null=True)
 
     def __str__(self):
         return self.title
--- book/views.py
+++ book/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.core.paginator import Paginator 
 from .models import Book
 
 def book_list_view(request):
     if request.method == 'GET':
         books_lst = Book.objects.all().order_by('-id')
-    return render(request, 'book_lst.html', {'books_lst': books_lst})
+        search_query = request.GET.get('search')
+        if search_query:
+            books_lst = books_lst.filter(title__icontains=search_query)
+        paginator = Paginator(books_lst, 3)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        
+    return render(request, 'book_lst.html', {'page_obj': page_obj, 'search_query': search_query})
 
 def book_detail_view(request, id):
     if request.method == 'GET':
         book_id = get_object_or_404(Book, id=id)
+        book_id.views += 1
+        book_id.save()
     return render(request, 'book_detail.html', {'book_id': book_id})
+
 
 
 

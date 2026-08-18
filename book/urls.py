39987ec8a09b558e1c4514160efdf39d4a77c51d
Azyaa <saaz.sr.rs@gmail.com>
2026-08-18 07:10:02 +0600

    дз 7v2

--- book/urls.py
+++ book/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('favourite/', views.HelloWorldView.as_view()),
     path('about/', views.AboutView.as_view()),
     path('games/', views.FavouriteGamesView.as_view()),
-    path('', views.BookListView.as_view(), name='book_list'),
+    path('book_list/', views.BookListView.as_view(), name='book_list'),
     path('book_list/<int:id>/', views.BookDetailView.as_view(), name='book_detail'),
 ]
